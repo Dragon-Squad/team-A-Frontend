f3d6cd37d064539dc711fcd330ca1fc7b9234a22
helixu38 <vipase2004@gmail.com>
2024-11-09 12:28:37 +0700

    feat: responsive added to hero section

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -3,12 +3,12 @@ import { Button } from "./button";
 
 function hero() {
   return (
-    <div className="flex flex-col w-full bg-primary-midnight-blue text-start pl-20 text-white h-screen ">
+    <div className="flex flex-col w-full items-center bg-primary-midnight-blue text-start text-white h-screen small-mobile:pl-10">
       <div className="mt-auto mb-auto space-y-5">
-        <h1 className="text-6xl font-semibold ">
+        <h1 className="md:text-6xl font-semibold small-mobile:text-5xl ">
           Empowering change <br></br> Through Collective Goodwill
         </h1>
-        <p>
+        <p className="text-base font-light">
           Join Charitan Today to discover and support charity initiatives that{" "}
           <br />
           resonate with your values, and help create a better world for all
