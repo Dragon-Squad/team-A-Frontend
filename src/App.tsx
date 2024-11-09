1c667f80959d04facb21b9c79e70576e330194e6
helixu38 <vipase2004@gmail.com>
2024-11-09 13:08:38 +0700

    feat: navigation-bar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./components/ui/hero";
+import NavigationBar from "./components/ui/navigation-bar";
 
 function LandingPage() {
   return (
     <>
+      <NavigationBar />
       <Hero />
     </>
   );
---
+++ src/components/ui/navigation-bar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavigationBar() {
+  return (
+    <div className="shadow-md w-full bg-primary-midnight-blue">
+      <div className="md:px-10 py-4 px-7">
+        {/*logo Icon */}
+        <div>Chartian</div>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
