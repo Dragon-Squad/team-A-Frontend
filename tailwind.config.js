f6302e4aeb5a6de9a9556dae327fe266e075e632
helixu38 <vipase2004@gmail.com>
2024-11-09 11:43:06 +0700

    style: add primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-midnight-blue": "#384959",
+        "primary-sky-blue": "#88BDF2",
+        "primary-pale-blue": "#BDDDFC",
+        "primary-steel-blue": "#6A89A7",
+        "secondary-soft-green": "#BADBA2",
+        "secondary-coral-pink": "#E39A7B",
+        "secondary-peach": "#FFD3AC",
+        "secondary-golden-sand": "#DBB06B",
+      },
+    },
   },
   plugins: [],
 };
