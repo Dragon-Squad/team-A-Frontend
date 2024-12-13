1ccd53a50a78a73050a467324d90cbbcbaa476fe
helixu38 <vipase2004@gmail.com>
2024-12-14 06:51:55 +0700

    feat: lazy loading form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
-import { AuthForm } from "./components/auth/authform";
+
+const AuthForm = React.lazy(() =>
+  import("./components/auth/authform").then((module) => ({
+    default: module.AuthForm,
+  })),
+);
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/register" element={<AuthForm mode="register" />} />
-      <Route path="/login" element={<AuthForm mode="login" />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<AuthForm mode="register" />} />
+        <Route path="/login" element={<AuthForm mode="login" />} />
+      </Routes>
+    </Suspense>
   );
 };
 
