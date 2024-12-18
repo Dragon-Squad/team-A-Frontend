abfb358fb859c6e694f3d2a340becd75fc96a5e9
helixu38 <vipase2004@gmail.com>
2024-12-18 09:05:00 +0700

    feat: added nav bar for the auth pages

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { AuthForm } from "../../components/auth/authform";
 import { Card } from "../../components/ui/card";
 import Logo from "../../components/logo";
+import NavigationBar from "@/components/ui/navigation-bar";
 
 const RegisterPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <Card className="w-full max-w-xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-        <div className="flex justify-center mb-6">
-          <Logo type={"logo1"} width={300} height={240} />
-        </div>
-        <AuthForm mode="register" />
-      </Card>
-    </div>
+    <>
+      <NavigationBar />
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <Card className="w-full max-w-xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+          <div className="flex justify-center mb-6">
+            <Logo type={"logo1"} width={300} height={240} />
+          </div>
+          <AuthForm mode="register" />
+        </Card>
+      </div>
+    </>
   );
 };
 
