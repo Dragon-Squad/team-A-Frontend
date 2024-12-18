e1a72a7793409ef3af8d4902b3a432683abe6850
shirin44 <S3983427@rmit.edu.vn>
2024-12-18 10:48:07 +0400

    feat: add footer component

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,12 +1,14 @@
 import "../../App.css";
 import Hero from "../../components/ui/hero";
 import NavigationBar from "../../components/ui/navigation-bar";
+import Footer from "../../components/ui/footer";
 
 function LandingPage() {
   return (
     <>
       <NavigationBar />
       <Hero />
+      <Footer />
     </>
   );
 }
