d04e6018286f602af02a8b71a7bab4cf35e0e4fb
helixu38 <vipase2004@gmail.com>
2024-12-18 09:03:49 +0700

    feat: added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landing/LandingPage";
-import LoginPage from "./pages/auth/LoginPage";
-import RegisterPage from "./pages/auth/RegisterPage";
-import ForgotPage from "./pages/auth/ForgotPassword";
+
+const LandingPage = React.lazy(() => import("./pages/landing/LandingPage"));
+const LoginPage = React.lazy(() => import("./pages/auth/LoginPage"));
+const RegisterPage = React.lazy(() => import("./pages/auth/RegisterPage"));
+const ForgotPage = React.lazy(() => import("./pages/auth/ForgotPassword"));
 
 const AuthForm = React.lazy(() =>
   import("./components/auth/authform").then((module) => ({
@@ -18,7 +19,7 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/login" element={<ForgotPage />} />
+        <Route path="/forgot" element={<ForgotPage />} />
       </Routes>
     </Suspense>
   );
