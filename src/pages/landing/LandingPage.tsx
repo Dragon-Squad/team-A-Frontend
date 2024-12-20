037dc85d985841cf0a23066a8520994d53cc303e
shirin44 <S3983427@rmit.edu.vn>
2024-12-20 13:44:06 +0400

    feat: implement hero section

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -2,11 +2,12 @@ import "../../App.css";
 import Hero from "../../components/ui/hero";
 import NavigationBar from "../../components/ui/navigation-bar";
 import Footer from "../../components/ui/footer";
-
+import HeroSection from "@/components/ui/HeroSection";
 function LandingPage() {
   return (
     <>
       <NavigationBar />
+      <HeroSection />
       <Hero />
       <Footer />
     </>
