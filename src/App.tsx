696ea2d4129f000f634c6f1dd01b37cda5299243
shirin44 <S3983427@rmit.edu.vn>
2025-01-09 17:16:44 +0700

    feat: add header and sidebar components to CharityView

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PaymentSuccessPage from "./pages/payment/PaymentSuccessPage";
 import ProjectDetailsPage from "./pages/project/ProjectDetails";
 import ProjectsPage from "./pages/project/ProjectPage";
 import ContactUsPage from "./pages/ContactUsPage";
+import CharityView from "./pages/Charity/CharityView";
 
 const LandingPage = React.lazy(() => import("./pages/landing/LandingPage"));
 const LoginPage = React.lazy(() => import("./pages/auth/SignInPage"));
@@ -26,6 +27,7 @@ const App: React.FC = () => {
         <Route path="/success" element={<PaymentSuccessPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<ContactUsPage />} />
+        <Route path="/charity" element={<CharityView />} />
       </Routes>
     </Suspense>
   );
