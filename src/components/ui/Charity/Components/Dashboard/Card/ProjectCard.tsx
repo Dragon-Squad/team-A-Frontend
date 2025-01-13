53a3a9ee6bd24f6cf847341ee0f714e7f2ddac96
helixu38 <vipase2004@gmail.com>
2025-01-14 04:15:25 +0700

    feat: style the project card

--- src/components/ui/Charity/Components/Dashboard/Card/ProjectCard.tsx
+++ src/components/ui/Charity/Components/Dashboard/Card/ProjectCard.tsx
@@ -4,6 +4,7 @@ import { Button } from "@headlessui/react";
 import { EditProjectDialog } from "../Dialog/EditProjectDialog";
 import { ProjectsCardProps } from "@/types/project";
 import { getLocalStorageItem } from "@/utils/helper";
+import { Progress } from "@/components/ui/progress";
 
 const ProjectCard: React.FC<ProjectsCardProps> = ({
   project,
@@ -16,6 +17,8 @@ const ProjectCard: React.FC<ProjectsCardProps> = ({
     const role = getLocalStorageItem<string>("userRole");
     setUserRole(role);
   }, []);
+
+  const progressPercentage = (project.raisedAmount / project.goalAmount) * 100;
 
   return (
     <Card
@@ -52,8 +55,9 @@ const ProjectCard: React.FC<ProjectsCardProps> = ({
         </CardContent>
       </div>
       <div className="p-6">
+        <Progress value={progressPercentage} className="mt-2" />
         <Button
-          className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800"
+          className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 mt-4"
           onClick={() => (window.location.href = `/details/${project._id}`)}
         >
           View Details
